908cb7b423134878e9928c39621d906348743144
Igor Cunha <67470515+igor3118@users.noreply.github.com>
2025-09-24 02:08:04 +0000

    conclusão

--- ex.java
+++ ex.java
@@ -1,13 +1,12 @@
-import java.util.scanner;
-public scanner input = new scanner(system.in);
-final int TAM = 30;
+import java.util.Scanner;
 public class ex {
+    static Scanner input = new Scanner(System.in);
+    static final int TAM = 30;
     public static void main(String[] args) {
         int vetor1[] = new int[TAM];
         int vetor2[] = new int[TAM];
-        criaVetor(vetor1);
-        criaVetor(vetor2);
-
+        int tam1 = 0, tam2 = 0;
+        int opcao;
         do{
             System.out.println("escolha uma opcao:");
             System.out.println("1 - inserir 1 elemento no conjunto A");
@@ -17,49 +16,51 @@ public class ex {
             System.out.println("5 - imprimir a diferença dos conjuntos A e B");
             System.out.println("6 - imprimir a diferença dos conjuntos B e A");
             System.out.println("0 - sair");
-            int opcao = input.nextInt();
+            opcao = input.nextInt();
             switch(opcao){
-                case 1:inserirElemento(vetor1, TAM);
+                case 1:tam1= inserirElemento(vetor1, tam1);
                 break;
-                case 2:{inserirElemento(vetor2, TAM);
-                        inserirElemento(vetor2, TAM);}
+                case 2:{tam2 = inserirElemento(vetor2, tam2);}
+                        
                 break;
-                case 3:{imprimirVetor(vetor1, TAM);
-                        imprimirVetor(vetor2, TAM);}
+                case 3:{imprimirVetor(vetor1, tam1);
+                        imprimirVetor(vetor2, tam2);}
                 break;
-                case 4:intersecao(vetor1, vetor2, TAM);
+                case 4:intersecao(vetor1,tam1, vetor2, tam2);
                 break;
-                case 5:diferençaAB(vetor1, vetor2, TAM);
+                case 5:diferenca(vetor1, tam1, vetor2, tam2, "A-B");
                 break;
-                case 6:diferençaBA(vetor1, vetor2, TAM);
+                case 6:diferenca(vetor2, tam2, vetor1, tam1, "B-A");
                 break;
                 case 0:System.out.println("saindo...");
                 break;
                 default:System.out.println("opcao invalida");
             }
         }while(opcao != 0);
+    }
+    
 
-    }
-    }
-    public class void criaVetor(int vetor[]){
-        for(int i = 0; i < TAM; i++){
-            vetor[i] = 0;
-        }
-    }
-    public class void inserirElemento(int v[], int tam){
-        System.out.println("informe um valor:");
-        int valor = input.nextInt();
-        int pos = buscaSequencial(v, tam, 0);
-        if(pos != -1){
-            v[pos] = valor;
+    public static int inserirElemento(int v[], int tam){
+        int valor;
+        System.out.println("informe o valor a ser inserido:");
+        valor = input.nextInt();
+        int pos = buscaSequencial(v, tam, valor);
+        if(pos == -1){
+            v[tam] = valor;
             System.out.println("valor inserido com sucesso");
-            System.out.println("tamanho atual do vetor: " + (pos + 1));
-        }else{
-            System.out.println("vetor cheio");
-
+            return tam + 1;
+        }else
+        if (tam >= v.length){
+            System.out.println("conjunto cheio, nao e possivel inserir novos elementos");
+            return tam;}
+            {
+            System.out.println("valor ja existe no conjunto");
+            return tam;
+        }
+        
     }
 
-    public class int buscaSequencial(int v[], int tam, int valor){
+    public static int buscaSequencial(int v[], int tam, int valor){
         for(int i = 0; i < tam; i++){
             if(v[i] == valor){
                 return i;
@@ -68,45 +69,41 @@ public class ex {
         return -1;
     }
 
-    public class void imprimirVetor(int v[], int tam){
-        for(int i = 0; i < tam; i++){
-            if(v[i] != 0){
-                System.out.println(v[i] + " ");
-            }
+    public static void imprimirVetor(int v[], int tam){
+        if (tam == 0) {
+            System.out.println("[vazio]");
+            return;
         }
-        System.out.println();
+        System.out.print("[ ");
+        for (int i = 0; i < tam; i++) {
+            System.out.print(v[i] + " ");
+        }
+        System.out.println("]");
     }
-    public class void intersecao(int v1[], int v2[], int tam){
-        System.out.println("interseção dos conjuntos A e B:");
-        for(int i = 0; i < tam; i++){
-            if(v1[i] != 0){
-                if(buscaSequencial(v2, tam, v1[i]) != -1){
-                    System.out.println(v1[i] + " ");
-                }
+    
+    public static void intersecao(int v1[], int tam1, int v2[], int tam2) {
+        System.out.print("Interseção (A ∩ B): [ ");
+        boolean encontrou = false;
+        for (int i = 0; i < tam1; i++) {
+            if (buscaSequencial(v2, tam2, v1[i]) != -1) {
+                System.out.print(v1[i] + " ");
+                encontrou = true;
             }
         }
-        System.out.println();
+        if (!encontrou) {
+            System.out.print("vazio");
+        }
+        System.out.println("]");
     }
-    public class void diferençaAB(int v1[], int v2[], int tam){
-        System.out.println("diferença dos conjuntos A e B:");
-        for(int i = 0; i < tam; i++){
-            if(v1[i] != 0){
-                if(buscaSequencial(v2, tam, v1[i]) == -1){
-                    System.out.println(v1[i] + " ");
-                }
-            }
+
+    public static void diferenca(int v1[], int tam1, int v2[], int tam2, String titulo) {
+       if (tam1==tam2) {
+        int [] v3 = new int[tam1];
+        for (int i = 0; i < tam1; i++) {
+            v3[i] = v1[i] - v2[i];
         }
-        System.out.println();
-    }
-    public class void diferençaBA(int v1[], int v2[], int tam){
-        System.out.println("diferença dos conjuntos B e A:");
-        for(int i = 0; i < tam; i++){
-            if(v2[i] != 0){
-                if(buscaSequencial(v1, tam, v2[i]) == -1){
-                    System.out.println(v2[i] + " ");
-                }
-            }
-        }
-        System.out.println();
+        System.out.println("Diferença ");
+        imprimirVetor(v3, tam1);
+       }
     }
 }
